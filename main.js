4167686632a0b529103ac8d12e693c19532e7688
james <emperor2g@gmail.com>
2023-09-20 19:33:24 +0100

    javascript plus and minus

--- main.js
+++ main.js
@@ -4,3 +4,25 @@ window.addEventListener( 'scroll',() => {
   document.querySelector('nav').classList.toggle
   ('window-scroll', window.scrollY > 0)
 })
+
+// show and hide FAQ answers
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('open');
+
+    // //changes icon
+
+    const icon = faq.querySelector('.faq-icon i');
+    if(icon.className =='fa-solid fa-plus'){
+      icon.className = "fa-solid fa-minus";
+    }
+    else
+     {
+      icon.className = "fa-solid fa-plus";
+    }
+
+  })
+})
